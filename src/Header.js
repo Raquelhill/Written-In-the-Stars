6530ae897436004ba43faccd4640e3c15f5be9ad
Raquel Hill <raquelturing@gmail.com>
2021-11-11 16:46:17 -0700

    Initial styling for dropdown

--- src/Header.js
+++ src/Header.js
@@ -13,46 +13,43 @@ const Header = () => {
         </h1>
       </div>
       <DropdownButton id="dropdown-basic-button" title="ZODIAC SIGNS">
-        <Dropdown.Item href="#/action-1">Aries</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Taurus</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Gemini</Dropdown.Item>
-        <Dropdown.Item href="#/action-4">Cancer</Dropdown.Item>
-        <Dropdown.Item href="#/action-5">Leo</Dropdown.Item>
-        <Dropdown.Item href="#/action-6">Virgo</Dropdown.Item>
-        <Dropdown.Item href="#/action-7">Libra</Dropdown.Item>
-        <Dropdown.Item href="#/action-8">Scorpio</Dropdown.Item>
-        <Dropdown.Item href="#/action-9">Sagittarius</Dropdown.Item>
-        <Dropdown.Item href="#/action-10">Capricorn</Dropdown.Item>
-        <Dropdown.Item href="#/action-11">Aquarius</Dropdown.Item>
-        <Dropdown.Item href="#/action-12">Pisces</Dropdown.Item>
+        <Dropdown.Item className="drop-down" href="#/action-1">
+          Aries
+        </Dropdown.Item>
+        <Dropdown.Item className="drop-down" href="#/action-2">
+          Taurus
+        </Dropdown.Item>
+        <Dropdown.Item className="drop-down" href="#/action-3">
+          Gemini
+        </Dropdown.Item>
+        <Dropdown.Item className="drop-down" href="#/action-4">
+          Cancer
+        </Dropdown.Item>
+        <Dropdown.Item className="drop-down" href="#/action-5">
+          Leo
+        </Dropdown.Item>
+        <Dropdown.Item className="drop-down" href="#/action-6">
+          Virgo
+        </Dropdown.Item>
+        <Dropdown.Item className="drop-down" href="#/action-7">
+          Libra
+        </Dropdown.Item>
+        <Dropdown.Item className="drop-down" href="#/action-8">
+          Scorpio
+        </Dropdown.Item>
+        <Dropdown.Item className="drop-down" href="#/action-9">
+          Sagittarius
+        </Dropdown.Item>
+        <Dropdown.Item className="drop-down" href="#/action-10">
+          Capricorn
+        </Dropdown.Item>
+        <Dropdown.Item className="drop-down" href="#/action-11">
+          Aquarius
+        </Dropdown.Item>
+        <Dropdown.Item className="drop-down" href="#/action-12">
+          Pisces
+        </Dropdown.Item>
       </DropdownButton>
-      <Navbar variant="dark" bg="dark" expand="lg">
-        <Container fluid>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbar-dark-example" />
-          <Navbar.Collapse id="navbar-dark-example">
-            <Nav>
-              <NavDropdown
-                id="nav-dropdown-dark-example"
-                title="Dropdown"
-                menuVariant="dark"
-              >
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
       <NavLink exact to="/" className="all-signs-nav">
         HOME
       </NavLink>
