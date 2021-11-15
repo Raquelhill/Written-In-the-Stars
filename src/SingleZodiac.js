46811e9494335f5c00843f09203e1acc49c5f95a
Raquel Hill <raquelturing@gmail.com>
2021-11-15 16:29:23 -0700

    Create and pass cypress tests

--- src/SingleZodiac.js
+++ src/SingleZodiac.js
@@ -42,25 +42,41 @@ class SingleZodiac extends Component {
         <>
           <section className="sign-details">
             <div className="sign-styling">
-              <p className="current-sign">{this.props.id}</p>
-              <p className="current-sign">{this.state.json.date_range}</p>
+              <p className="current-sign current-sign-title">{this.props.id}</p>
+              <p className="current-sign date_range">
+                {this.state.json.date_range}
+              </p>
               <div className="horoscope-styling">
-                <p>Daily Horoscope: {this.state.json.current_date} </p>
+                <p className="current_date">
+                  Daily Horoscope: {this.state.json.current_date}{' '}
+                </p>
                 <p className="description">{this.state.json.description}</p>
-                <p>Compatability: {this.state.json.compatibility}</p>
-                <p>Mood: {this.state.json.mood} </p>
-                <p>Spirit Color: {this.state.json.color} </p>
-                <p>Lucky Number: {this.state.json.lucky_number} </p>
-                <p>Lucky Time of Day: {this.state.json.lucky_time} </p>
+                <p className="compatibility">
+                  Compatability: {this.state.json.compatibility}
+                </p>
+                <p className="mood">Mood: {this.state.json.mood} </p>
+                <p className="color">Spirit Color: {this.state.json.color} </p>
+                <p className="lucky_number">
+                  Lucky Number: {this.state.json.lucky_number}{' '}
+                </p>
+                <p className="lucky_time">
+                  Lucky Time of Day: {this.state.json.lucky_time}{' '}
+                </p>
                 <div className="day-button-styling">
                   <Link to={`/${this.props.id}/yesterday`}>
-                    <button className="day-button">Yesterday</button>
+                    <button className="day-button yesterday-day-button">
+                      Yesterday
+                    </button>
                   </Link>
                   <Link to={`/${this.props.id}/today`}>
-                    <button className="day-button">Today</button>
+                    <button className="day-button today-day-button">
+                      Today
+                    </button>
                   </Link>
                   <Link to={`/${this.props.id}/tomorrow`}>
-                    <button className="day-button">Tomorrow</button>
+                    <button className="day-button tomorrow-day-button">
+                      Tomorrow
+                    </button>
                   </Link>
                 </div>
               </div>
